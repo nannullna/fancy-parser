7775e58659c18ac9f9d79fb7030e0fd012f65241
nannullna <nannullna00@gmail.com>
2023-08-26 14:00:25 +0900

    remove and move files

--- parse.py
+++
@@ -1,9 +0,0 @@
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-parser.add_argument('--config', type=str, default=None, help='Path to config file.')
-args, remaining_args = parser.parse_known_args(["--epochs", "20"])
-
-print(args)
-print(remaining_args)
-
